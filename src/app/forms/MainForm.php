aa8accf3592693e896413b3b62b0b67c5ad33884
Komdu <komdu228p@gmail.com>
2025-04-02 19:41:36 +0300

    Add files via upload

---
+++ src/app/forms/MainForm.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+    /**
+     * @event mediaView.construct 
+     */
+    function doMediaViewConstruct(UXEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event keyDown-N 
+     */
+    function doKeyDownN(UXKeyEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event keyDown-M 
+     */
+    function doKeyDownM(UXKeyEvent $e = null)
+    {    
+        
+    }
+
+
+}
